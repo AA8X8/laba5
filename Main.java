f01027cd7212032b026ccce62550c31562693625
AA8X8 <areon.aur@mail.ru>
2025-03-24 14:01:21 +0300

    update 5

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("update 3");
+        System.out.println("update 5");
     }
 }
